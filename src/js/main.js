e30a391a0bf3628b3c765d6b4499924cc93aba8a
obedvav <obedvav@gmail.com>
2019-08-25 07:51:20 -0500

    Agregado el botón de cerrar en el menú lateral (hace falta centrarlo bien)

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
-var menuIcon = document.getElementById("menuIcon")
-
 var menuOpen = new TimelineMax({paused:false, reversed:true})
     
     menuOpen
-    .to("#menuIcon", .25, {transformOrigin: "right 50px", ease:Power2.easeInOut}, "cross")
     .to("#menu2", 0.25, {width: "350px", ease:Power2.easeInOut})
     
     function menuIn() {
