1c1efad8879855dfd05973d4e25db1e18a235181
Pablo <alanvquez186@gmail.com>
2019-08-25 21:57:49 -0500

    Index final

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 var menuOpen = new TimelineMax({paused:false, reversed:true})
     
     menuOpen
-    .to("#menu2", 0.25, {width: "350px", ease:Power2.easeInOut})
+    .to("#menu2", 0.5, {width: "100%", padding: "50px 0", ease:Power2.easeInOut})
     
     function menuIn() {
         menuOpen.reversed() ? menuOpen.play() : menuOpen.reverse(); 
